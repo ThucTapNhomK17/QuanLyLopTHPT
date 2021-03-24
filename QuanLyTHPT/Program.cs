6008feaffd1d0210a386c6331f36eb82b2d6132f
Anger000 <77683727+khue24112000@users.noreply.github.com>
2021-03-24 08:21:04 +0700

    Finished V2

--- QuanLyTHPT/Program.cs
+++ QuanLyTHPT/Program.cs
@@ -17,6 +17,7 @@ namespace QuanLyTHPT
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //chinh sua finish v2
         }
     }
 }
